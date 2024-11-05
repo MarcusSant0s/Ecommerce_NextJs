1b4957674a2ff1f58bfe7c3a3de8b0a1e97c6ffb
marcus.pereira.paulo@gmail.com <marcus.pereira.paulo@gmail.com>
2024-11-04 21:04:08 -0300

    Deploy #1

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
-import { Inter } from "next/font/google";
+ 
 import "./globals.css";
-import Navbar from "../components/Navbar";
+import {Navbar} from "@/components";
 import {StateContext} from '@/context/StageContext'
 
 
