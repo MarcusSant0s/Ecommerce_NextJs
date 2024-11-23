3019e1d50b075ee0ac57177d2e2fd8aaf39b4647
marcus.pereira.paulo@gmail.com <marcus.pereira.paulo@gmail.com>
2024-11-23 17:18:42 -0300

    UPGRADE ProductsList

--- components/ProductsList.jsx
+++ components/ProductsList.jsx
@@ -1,13 +1,8 @@
 'use client'
 import { useEffect, useState } from 'react';
-import { Product } from '@/components'
-
-
-
+import { Product } from '@/components';
 
 const ProductsList = ({ FirstProducts }) => {
-
-
   const [products, setProducts] = useState([...FirstProducts]);
   const [start, setStart] = useState(50);
   const [loading, setLoading] = useState(false);
@@ -27,49 +22,28 @@ const ProductsList = ({ FirstProducts }) => {
     }
   };
 
-
-
   return (
-    <div style={{ width: 'calc(100vw - 3vw)' }} className="grid max-[380px]:grid-cols-1 rows--height grid-cols-2 sm:grid-cols-3 md:grid-cols-4 md:p-2 md:gap-2    border rounded-md border-dashed w-full h-max pt-2 gap-y-4">
+    <div style={{ width: 'calc(100vw - 3vw)' }} className="grid max-[380px]:grid-cols-1 rows--height grid-cols-2 sm:grid-cols-3 md:grid-cols-4 md:p-2 md:gap-2 mx-auto border rounded-md border-dashed w-full h-max pt-2 gap-y-4">
+      
+      {products.length > 0 ? (
+        products.map((productsData) => (
+          <Product key={productsData._id} product={productsData} />
+        ))
+      ) : (
+        <p>No products found for this page.</p>
+      )}
 
-
-        {products.length > 0 ? (
-                products.map((productsData) => (
-                  <Product key={productsData._id} product={productsData} />
-
-
-                ))
-
-              ) : (
-
-                <p>No products found for this page.</p>
-
-              )}
-        {products.length > 0 ? (
-                products.map((productsData) => (
-                  <Product key={productsData._id} product={productsData} />
-
-
-                ))
-
-              ) : (
-
-                <p>Em manutenção.</p>
-
-              )}
-
-
-
-
-
-      <button className="col-span-full flex justify-center " onClick={loadMoreProducts} disabled={loading}>
-
-        <div className="border border-red-500 bg-red-400 text-white   rounded-sm font-bold py-4 px-6  flex items-center">
+      <button 
+        className="col-span-full flex justify-center" 
+        onClick={loadMoreProducts} 
+        disabled={loading}
+      >
+        <div className="border bg-pink-600 text-white rounded-sm font-bold py-4 px-6 flex items-center">
           {loading ? "Carregando..." : "Ver mais"}
         </div>
       </button>
     </div>
-  )
+  );
 }
 
-export default ProductsList
+export default ProductsList;
